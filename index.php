ad48685cf220fddfc71c27f2258afff449c1acb9
goorus <goorus@list.ru>
2012-11-28 23:43:17 +0400

    # Сокращен index.php

--- index.php
+++ index.php
@@ -7,14 +7,12 @@
 $pathes = array(
     __DIR__ . '/../../ruon.local/www/'
 );
-$ruonPath = null;
 
 foreach ($pathes as $path) {
     if (is_dir($path)) {
-        $ruonPath = $path;
+        require $path . '/Ruon/Core.php';
         break;
     }
 }
 
-require $ruonPath . '/Ruon/Core.php';
 Ruon\Core::initApp(__DIR__ . '/Double', 'Double')->run();
